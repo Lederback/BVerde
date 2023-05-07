7a27a9639ac18c5da4232f1687b0362c83a3177b
dtonavitor <vitor.barros@sou.inteli.edu.br>
2023-05-07 05:11:27 -0300

    feat: handling empty inputs

--- frontend/src/components/login/forms.js
+++ frontend/src/components/login/forms.js
@@ -1,12 +1,22 @@
 import styles from './forms.module.css'
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export default function Forms(){
     const router = useRouter();
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
+    const [notEmail, setNotEmail] = useState(true);
+    const [notPassword, setNotPassword] = useState(true);
+
     const handleSubmit = (event) =>{
         event.preventDefault();
-        router.push('/about')
+        if (email && password) {
+            router.push('/about')
+        }
     }
 
     return(
@@ -19,19 +29,29 @@ export default function Forms(){
                 <div className={styles.info}>
                     <img src='/user.png' className={styles.icon}/>
                     <div className={styles.usernameForms}>
-                        <p>Usuário:</p>
-                        <input className={styles.input} />
+                        <p>Email:</p>
+                        <input className={styles.input} value={email} onChange={(e) => {
+                            setEmail(e.target.value);
+                            (e.target.value.length <= 0) ? setNotEmail(true) : setNotEmail(false);
+
+                            (notEmail || notPassword) ? setIsButtonDisabled(true) : setIsButtonDisabled(false);
+                        }}/>
                     </div>
                 </div>
                 <div className={styles.info}>
                     <img src='/lock.png' className={styles.icon2}/>
                     <div className={styles.passwordForms}>
                         <p>Senha:</p>
-                        <input className={styles.input} />
+                        <input className={styles.input} value={password} onChange={(e) => {
+                            setPassword(e.target.value);
+                            (e.target.value.length <= 0) ? setNotPassword(true) : setNotPassword(false);
+
+                            (notEmail || notPassword) ? setIsButtonDisabled(true) : setIsButtonDisabled(false)
+                        }}/>
                     </div>
                 </div>
                 <div className={styles.submit}>
-                    <input type='submit' className={styles.button}/>
+                    <input type='submit' className={styles.button} disabled={isButtonDisabled}/>
                 </div>
             </form>
         </div>
